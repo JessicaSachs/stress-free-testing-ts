0f8c5a22eaa5237535d79718d1b859e91ed7b193
Jessica Sachs <jess@jessicasachs.io>
2021-11-21 04:09:10 -0500

    simple mount

--- src/components/HelloWorld.cy.jsx
+++ src/components/HelloWorld.cy.jsx
@@ -1,3 +1,30 @@
-it('works', () => {
-  expect(true).to.be.true
+import { mount } from '@cypress/vue'
+import HelloWorld from './HelloWorld.vue'
+
+// Reusable Selectors
+const textSelector = '[data-testid=message]'
+
+// Constants
+const msg = 'Welcome to Stress-free Testing with Vue 3!'
+
+describe('<HelloWorld />', () => {
+  it('renders', () => {
+    // The first argument is the component to be mounted.
+    // The second argument is optional and is used for passing in props,
+    // plugins, etc. The complete documentation for this signature can be
+    // found in the Vue Test Utils docs.
+
+    mount(HelloWorld, { props: { msg } })
+      .get(textSelector)
+      .should('contain.text', msg)
+  })
+})
+
+it.skip('JSX syntax example', () => {
+  // The JSX Syntax for Vue is less documented, however it will be very useful later
+  // on when we begin to use wrapper components to test slots and CSS.
+
+  mount(<HelloWorld msg={msg} />)
+    .get(textSelector)
+    .should('contain.text', msg)
 })
