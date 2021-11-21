3dd9365c9874773c8f784365daf4252e83775928
Jessica Sachs <jess@jessicasachs.io>
2021-11-21 18:08:52 -0500

    scoped slots

---
+++ src/components/Accordion.cy.jsx
@@ -0,0 +1,25 @@
+import { mount } from '@cypress/vue'
+import Accordion from './Accordion.vue'
+
+const headerSelector = '[data-testid=header]'
+const contentSelector = '[data-testid=content]'
+
+describe('<Accordion />', () => {
+  it('renders', () => {
+    const slots = {
+      default: () => <p data-testid="content">Content Within the slot</p>,
+      target: () => <h1 data-testid="header">Header</h1>,
+    }
+
+    mount(() => (
+      <div class="p-12">
+        <h1 class="text-lg">Accordion Examples</h1>
+        <h1 class="pb-8">Click on the header to toggle the card</h1>
+        <Accordion vSlots={slots}></Accordion>
+      </div>
+    ))
+
+    // Use the header selectors and content selectors to interact
+    // with the accordion and test that it correctly expands and collapses.
+  })
+})
---
+++ src/components/Accordion.vue
@@ -0,0 +1,38 @@
+<script setup>
+import { ref } from 'vue'
+
+const props = defineProps({
+  collapseInitially: {
+    type: Boolean,
+    default: false
+  },
+  width: {
+    type: String,
+    default: '280px'
+  }
+})
+
+
+const collapsed = ref(props.collapseInitially)
+</script>
+
+<template>
+  <button
+    @click="collapsed = !collapsed"
+    class="p-2 transition duration-150 rounded outline-none focus-within:outline-none ring-2 ring-transparent hover:ring-blue-200 focus:ring-blue-200 hover:border-blue-300 focus:border-blue-300"
+    :style="{ width, textAlign: 'initial' }"
+  >
+    <div class="inline-flex items-center">
+      <slot name="target" />
+      <i-mdi-chevron-down
+        class="inline transition duration-150 transform"
+        :class="{
+          'rotate-180': collapsed
+        }"
+      />
+    </div>
+    <div v-if="collapsed" class="pt-2">
+      <slot />
+    </div>
+  </button>
+</template>
