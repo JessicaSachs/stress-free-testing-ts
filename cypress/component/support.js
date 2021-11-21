5f5aac714a4d20d1fd82f079a2ed189599e96199
Jessica Sachs <jess@jessicasachs.io>
2021-11-21 04:07:20 -0500

    the beginning

--- cypress/component/support.js
+++ cypress/component/support.js
@@ -2,12 +2,3 @@
 import '@testing-library/cypress/add-commands'
 
 // The Cypress support.js file is loaded *once per spec* and contains global setup and teardown.
-
-// *Workshop Task*: Fix the application's styles.
-
-// 1. Look inside of src/main.js and import any other global styles.
-import 'virtual:windi.css'
-import 'virtual:windi-devtools'
-
-// 2. Import main.scss here.
-import '../../src/main.scss'
