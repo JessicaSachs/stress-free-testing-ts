182e9b594f27802e74748b966894eb2a1b631acb
Jessica Sachs <jess@jessicasachs.io>
2022-06-07 22:55:04 -0400

    adding support file

--- src/App.cy.jsx
+++ src/App.cy.jsx
@@ -1,7 +1,12 @@
 import App from './App.vue'
 
+const counterSelector = '[data-testid=counter]'
+
 describe('<App />', () => {
   it('renders', () => {
     cy.viewport(800, 600)
+    cy.mount(App)
+    cy.findByText('Demo').click()
+    cy.get(counterSelector).should('be.visible')
   })
 })
