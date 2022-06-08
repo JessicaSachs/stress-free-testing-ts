a57135060970bcad723b9cdb66b2b7cf79a2954d
Jessica Sachs <jess@jessicasachs.io>
2022-06-07 22:55:04 -0400

    swapping to cy-10

--- cypress/component/plugins.js
+++
@@ -1,8 +0,0 @@
-const { startDevServer } = require('@cypress/vite-dev-server')
-
-module.exports = (on, config) => {
-  on('dev-server:start', (options) => {
-    return startDevServer({ options })
-  })
-  return config
-}
--- cypress/component/support.js
+++
@@ -1,4 +0,0 @@
-// Cypress Testing Library - https://testing-library.com/docs/cypress-testing-library/intro/
-import '@testing-library/cypress/add-commands'
-
-// The Cypress support.js file is loaded *once per spec* and contains global setup and teardown.
